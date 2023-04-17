91cab1b274e9b9aa3469da65b7d877c486f643e6
赵龙生 <zhaolongsheng@tenwit.com>
2023-04-17 17:18:22 +0800

    chore: cleanup

--- packages/vue/src/router/index.ts
+++ packages/vue/src/router/index.ts
@@ -7,11 +7,6 @@ const routes: RouteRecordRaw[] = [
     name: 'Home',
     component: () => import('@src/views/Home/index.vue'),
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('@src/views/About/index.vue'),
-  },
 ]
 
 export const router = createRouter({
