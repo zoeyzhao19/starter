4908b8e679ace50253abd92683e69d8cb900641b
zls <zls@work.com>
2025-01-23 10:04:20 +0800

    feat: 新增主题切换动画

--- packages/vue/src/composables/appearance.ts
+++ packages/vue/src/composables/appearance.ts
@@ -1,11 +1,51 @@
-export function switchDarkLightMode() {
-  const html = document.querySelector('html');
-  const isDark = html?.classList.contains('dark');
-  if (isDark) {
-    html?.classList.remove('dark');
-    html?.classList.add('light');
-  } else {
-    html?.classList.remove('light');
-    html?.classList.add('dark');
+import { nextTick, onMounted, ref, watchEffect } from "vue";
+
+function enableTransitions() {
+  return 'startViewTransition' in document
+    && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+}
+
+const darkMatches = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+export const useAppearance = () => {
+  const isDark = ref(darkMatches)
+
+  onMounted(() => {
+    watchEffect(() => {
+      document.documentElement.classList.toggle('dark', isDark.value)
+    })
+  })
+
+  async function toggleAppearance({ clientX: x, clientY: y }: MouseEvent) {
+    if (!enableTransitions()) {
+      isDark.value = !isDark.value
+      return
+    }
+
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${Math.hypot(
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y),
+      )}px at ${x}px ${y}px)`,
+    ]
+  
+    await document.startViewTransition(async () => {
+      isDark.value = !isDark.value
+      await nextTick()
+    }).ready
+  
+    document.documentElement.animate(
+      { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+      {
+        duration: 300,
+        easing: 'ease-in',
+        pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
+      },
+    )
+  }
+
+  return {
+    toggleAppearance
   }
 }
--- packages/vue/src/views/Home/index.vue
+++ packages/vue/src/views/Home/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { switchDarkLightMode } from '@src/composables';
+import { useAppearance } from '@src/composables';
 
 defineOptions({
   name: 'Home',
 });
+
+const { toggleAppearance } = useAppearance();
 </script>
 
 <template>
@@ -12,7 +14,7 @@ defineOptions({
       <p font="serif" text="3xl black dark:white" px="2"></p>
       <div flex="~" px="2">
         <a target="_blank" href="https://github.com/zoeyzhao19" class="cursor-pointer text-xl i-carbon-logo-github"></a>
-        <button class="ml-2 text-xl i-carbon-sun dark:i-carbon-moon" @click="switchDarkLightMode" />
+        <button class="ml-2 text-xl i-carbon-sun dark:i-carbon-moon" @click="toggleAppearance" />
       </div>
     </nav>
   </div>
